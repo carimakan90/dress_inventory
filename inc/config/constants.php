a73a8dfb901352413fb385f08f9842beaf5464e2
kinglaw90 <34879321+kinglaw90@users.noreply.github.com>
2025-01-19 22:44:15 +0800

    update database

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -1,21 +1,21 @@
 <?php
 	// Root url for the site
-	define('ROOT_URL', 'http://localhost/inventory/');
+	define('ROOT_URL', 'https://inventory2.aroundsia.com/inventory/');
 	
 	
 	// Database parameters
 	// Data source name
-	define('DSN', 'mysql:host=localhost;dbname=shop_inventory');
+	define('DSN', 'mysql:host=localhost;dbname=leedongr_shop_inventory');
 	
 	// Hostname
 	define('DB_HOST', 'localhost');
 	
 	// DB user
-	define('DB_USER', 'inventoryUser');
+	define('DB_USER', 'leedongr_inventoryUser');
 	
 	// DB password
-	define('DB_PASSWORD', 'password');
+	define('DB_PASSWORD', '5bn9q9eqy36d');
 	
 	// DB name
-	define('DB_NAME', 'shop_inventory');
+	define('DB_NAME', 'leedongr_shop_inventory');
 ?>
